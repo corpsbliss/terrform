441585432e6a929b9df1398672406f870840ee23
corpsbliss <100997302+corpsbliss@users.noreply.github.com>
2025-04-26 07:42:04 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,21 @@
+variable "key_name"{
+    
+    type = string
+    default = "rohit"
+
+}
+
+variable "ami_id"{
+    type = string
+    default = "qwerty"
+}
+
+variable "instance_type"{
+    type = string
+    default = "t2.micro"
+}
+
+variable "instance_name"{
+    type = string
+    default = "rohit2"
+}
